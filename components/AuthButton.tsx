32fbb1e59229f95d6c50005d5c99b8d2a6b0c3ec
배성완 <baesee@kakao.com>
2023-12-26 10:23:02 +0900

    spotify login

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,24 +1,25 @@
-import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function AuthButton() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
+  console.log(user);
 
   const signOut = async () => {
-    'use server'
+    "use server";
 
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-    await supabase.auth.signOut()
-    return redirect('/login')
-  }
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
 
   return user ? (
     <div className="flex items-center gap-4">
@@ -36,5 +37,5 @@ export default async function AuthButton() {
     >
       Login
     </Link>
-  )
+  );
 }
